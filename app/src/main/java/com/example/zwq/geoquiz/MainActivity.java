f1562b1fe2d8436f5c6108e3b6c6df8c16f56a1b
perfe-er <zhu063296@gmail.com>
2019-09-06 21:22:15 +0800

    1、xml新增了两个按钮 2、activity对这两个按钮进行监听点击事件并用toast显示

--- app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/zwq/geoquiz/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.zwq.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                Toast.makeText(this,"我是按钮1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this,"我是按钮2", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
